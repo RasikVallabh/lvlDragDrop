ad4eedbd72214387dc8eaaa3bf36e892f89af404
Rasik Vallabh <rasik23@gmail.com>
2018-05-02 06:52:51 +0530

    Add files via upload

--- dashboard.component.ts
+++ dashboard.component.ts
@@ -1,36 +1,117 @@
-import { Component, OnInit } from '@angular/core';
-import {BsDatepickerConfig} from 'ngx-bootstrap';
-import {
-  ReactiveFormsModule,
-  FormsModule,
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder, FormArray
-} from '@angular/forms';
+import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {BsDatepickerConfig, BsDropdownModule} from 'ngx-bootstrap';
+import {ProposalCountryModel} from './proposal.country.model';
+import {ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators, FormBuilder, FormArray} from '@angular/forms';
+import {SectorModel} from './sector.model';
+import {ProposalDelModel} from './proposal.model';
+import {ProposalSectorModel} from './proposal.sector.model';
+import {CountryModel} from './country.model';
+import {DATE} from 'ngx-bootstrap/chronos/units/constants';
+import {DatePipe} from '@angular/common';
+import {FileUploaderComponent} from '../file-uploader/file-uploader.component';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+
+  @Input() item: SectorModel;
   datePickerConfig: Partial<BsDatepickerConfig>
-  constructor(private fb: FormBuilder) {
+  public approvalForm: FormGroup;
+  app_sectorPlaceholder: string = 'Please Select Sector';
+  app_countryPlaceholder: string = 'Please Select Country';
+  proposalModel: ProposalDelModel;
+  proposalSectorModelList: ProposalSectorModel[];
+  proposalCountryModelList: ProposalCountryModel[];
+  sectorList: SectorModel[] = [new SectorModel('Sector1',12,'sectordesc'),new SectorModel('Sector2',12,'sectordesc'),
+    new SectorModel('Sector3',12,'sectordesc'),new SectorModel('Sector4',12,'sectordesc'),new SectorModel('Sector5',12,'sectordesc')];
+  countryList: CountryModel[] = [new CountryModel('country1',12,'countrydesc'),new CountryModel('country2',12,'countrydesc'),new CountryModel('country3',12,'countrydesc'),new CountryModel('Country4',12,'sectordesc'),new CountryModel('Country5',12,'sectordesc')];
+  sectorTemp: SectorModel;
+  @ViewChild('fileInput') fileInput: ElementRef;
+  /*MOCK DATA CREATION START*/
+
+  /*MOCK DATA CREATION END*/
+  constructor(private _fb: FormBuilder) {
+
   }
-  sectors: string[] = [
-    'English',
-    'French',
-    'German',
-  ];
-  approvalsForm: FormGroup;
-  formArray: FormArray;
   ngOnInit() {
-
-    this.approvalsForm = new FormGroup({
-      sector: new FormControl()
+    // we will initialize our form here
+    this.approvalForm = this._fb.group({
+      sectors: new FormArray([this.initSectors()]),
+      countries: new FormArray([this.initCountries()])
     });
+  }
+  initSectors() {
+    return this._fb.group({
+      app_sector_approvalName: new FormControl(''),
+      app_sector_approvalSector: new FormControl(SectorModel),
+      app_sector_approvalDate: new FormControl(new Date()),
+      avtar: new FormControl()
+    });
+  }
+  initCountries() {
+    return this._fb.group({
+      app_country_approvalName: new FormControl(''),
+      app_country_approvalCountries: new FormControl(''),
+      app_country_approvalDate: new FormControl(Date)
+    });
+  }
+  addSectors() {
+    // add address to the list
+    const control = <FormArray>this.approvalForm.controls['sectors'];
+    control.push(this.initSectors());
+  }
+  addCountry() {
+    // add address to the list
+    const control = <FormArray>this.approvalForm.controls['countries'];
+    control.push(this.initCountries());
+  }
+  removeSectors(i: number) {
+    // remove address from the list
+    const control = <FormArray>this.approvalForm.controls['sectors'];
+    control.removeAt(i);
+  }
+  selectedSector() {
 
-    formArray: this.fb.array([]);
+  }
+  onSubmit(): void {
+    console.log(this.approvalForm.controls.sectors.value);
+    console.log(this.approvalForm.value.sectors[0].app_sector_approvalSector);
+    this.sectorTemp = this.approvalForm.value.sectors[0].app_sector_approvalSector;
+    console.log(this.sectorTemp);
+  }
+  onChangeSectorSel(event) {
+    this.approvalForm.patchValue({app_sector_approvalSector: event });
+  }
+  onChangeCountrySel(event) {
+    this.approvalForm.patchValue({app_country_approvalCountries: event });
+    console.log('BEFORE:::' + this.approvalForm.value.sectors[0].app_sector_approvalDate);
+    this.approvalForm.patchValue({app_country_approvalDate:
+        new DatePipe(navigator.language)
+          .transform(this.approvalForm.value.sectors[0].app_sector_approvalDate, 'DD MMM YYYY') });
+    console.log('AFTER:::' + this.approvalForm.value.sectors[0].app_sector_approvalDate + ':::::::' + this.datePickerConfig.value);
   }
 
+  onFileChange(event) {
+    const reader = new FileReader();
+    console.log(this.approvalForm.controls.sectors);
+    if (event.target.files && event.target.files.length > 0) {
+      const file = event.target.files[0];
+      reader.readAsDataURL(file);
+      reader.onload = () => {
+        this.approvalForm.controls.sectors.get('avatar').setValue({
+          filename: file.name,
+          filetype: file.type,
+          value: reader.result.split(',')[1]
+        });
+      };
+    }
+  }
+
+  clearFile() {
+    this.approvalForm.get('avatar').setValue(null);
+    this.fileInput.nativeElement.value = '';
+  }
 }
